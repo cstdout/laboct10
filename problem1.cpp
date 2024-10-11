fa69a35db481b2324bfa51ac7b284fe311c177c6
cstdout <151808195+cstdout@users.noreply.github.com>
2024-10-11 00:30:57 -0700

    Create problem1.cpp

---
+++ problem1.cpp
@@ -0,0 +1,28 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+
+int main() {
+  
+    int age;
+    std::cout << "Enter your age ";
+    std::cin >> age;
+    
+    if(age >= 0 && age <= 14)
+    {
+        std::cout << "child";
+    }
+    else if(age >= 15 && age <= 24)
+    {
+        std::cout << "young";
+    }
+    else if(age >= 25 && age <= 64)
+    {
+        std::cout << "adult";
+    }
+    else if(age >= 60)
+    {
+        std::cout << "senior";
+    }
+    
+    return 0;
+}
